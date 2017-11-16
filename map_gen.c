7e7f8d714b0ce6d1ae0503bb46f2aa4f1ea1207e
John McCormick <johnmccormick91@gmail.com>
2017-11-16 13:36:18 +0000

    Added feature to advance and retreat through rooms with N (next) and P (previous), and you can quit with Q. Advancing opens up a new randomly generated room with the entrance on the opposite wall to the exit of the previous room. If you retreat, you visit the previous room, and if you advance from there, you go back in to the next room which retains it's map. You can advance and retreat as much as yyou like (or at least until you run out of memory).

--- map_gen.c
+++ map_gen.c
@@ -7,7 +7,6 @@ struct door
 {
 	int side;
 	int position;
-	int type;
 };
 
 struct level
@@ -15,80 +14,172 @@ struct level
 	int *map;
 	int width;
 	int height;
+	struct door entrance;
+	struct door exit;
+	struct level *prev_level;
+	struct level *next_level;
 };
 
 char sides[4] = "NESW";	
 char door_types[3] = "01X";	
 
-void assign_random_door_position (struct level *room, struct door _door)
+int main () 
 {
-	// North and South are 0 and 2
-	if (_door.side % 2 == 0)
-	{
-		// Give the door a 1 dimensional position that sits between the room's width on the N/S wall.
-		_door.position = (rand() / (RAND_MAX / (room->width - 2))) + 1;
-	} 
-	// East and West are 1 and 3
-	else
-	{
-		_door.position = (rand() / (RAND_MAX / (room->height - 2))) + 1;
-	}
+	// Max w/h should always be >= 3
+	// otherwise there can be no door
+	int MIN_LEVEL_WIDTH = 3; 
+	int MIN_LEVEL_HEIGHT = 3;
 
-	//printf("%i, %i\n", _door.side, _door.position);
+	int MAX_LEVEL_WIDTH = 20;
+	int MAX_LEVEL_HEIGHT = 20;
 
-	if (_door.side == 0)
-		*(room->map + _door.position) = _door.type;
-	else if (_door.side == 1)
-		*(room->map + (room->width - 1) + (_door.position * room->width)) = _door.type;
-	else if (_door.side == 2)
-		*(room->map + (room->width * (room->height - 1)) + _door.position)= _door.type;
-	else if (_door.side == 3)
-		*(room->map + (_door.position * room->width)) = _door.type;
-}
-
-
-void give_room_entrance_and_exit (struct level *room)
-{
 	struct timeval time;
 	gettimeofday(&time, NULL);
 	srand(time.tv_usec);
 
-	struct door entrance;
-	struct door exit;
+	struct level *current_level = malloc(sizeof(struct level));
 
-	entrance.type = 1;
-	entrance.side = rand() / (RAND_MAX / 4);
+	current_level->map = NULL;
+	current_level->next_level = NULL;
+	current_level->prev_level = NULL;
 
-	exit.type = 2;
+	current_level->entrance.side = rand() / (RAND_MAX / 4);
 
-	// I'm sure this could be more elegant
-	do {
-		exit.side = (rand() / (RAND_MAX / 4));
-	} while (exit.side == entrance.side);
-
-	assign_random_door_position(room, entrance);
-	assign_random_door_position(room, exit);
-}
-
-
-int main () 
-{
-	struct level room[1];
-
-	room[0].width = 6;
-	room[0].height = 8;
-
-	room[0].map = malloc(sizeof(int) * room[0].width * room[0].height);
-	memset(room[0].map, 0, sizeof(int) * room[0].width * room[0].height);
-
-	give_room_entrance_and_exit(&room[0]);
-
-	for (int y = 0; y < room[0].height; ++y)
+	int advance = 1;
+	// Game loop
+	for (;;)
 	{
-		for (int x = 0; x < room[0].width; ++x)
+		if (advance == 1)
 		{
-			printf("%c \t", door_types[*(room[0].map + (y * room[0].width) + x)]);
+
+			if (current_level->map == NULL)
+			{
+				current_level->width = rand() / (RAND_MAX / MAX_LEVEL_WIDTH) + MIN_LEVEL_WIDTH;
+				current_level->height = rand() / (RAND_MAX / MAX_LEVEL_HEIGHT) + MIN_LEVEL_HEIGHT;
+
+				// North and South are 0 and 2
+				if (current_level->entrance.side % 2 == 0)
+				{
+					// Give the door a 1 dimensional position that sits between the level's width on the N/S wall.
+					current_level->entrance.position = (rand() / (RAND_MAX / (current_level->width - 2))) + 1;
+				} 
+				// East and West are 1 and 3
+				else
+				{
+					current_level->entrance.position = (rand() / (RAND_MAX / (current_level->height - 2))) + 1;
+				}
+
+				current_level->map = malloc(sizeof(int) * current_level->width * current_level->height);
+				memset(current_level->map, 0, sizeof(int) * current_level->width * current_level->height);
+
+				// Place current level entrance on map according to side / position
+				if (current_level->entrance.side == 0)
+				{
+					*(current_level->map + current_level->entrance.position) = 1;
+				}
+				else if (current_level->entrance.side == 1)
+				{
+					*(current_level->map + (current_level->width - 1) + (current_level->entrance.position * current_level->width)) = 1;
+				}
+				else if (current_level->entrance.side == 2)
+				{
+					*(current_level->map + (current_level->width * (current_level->height - 1)) + current_level->entrance.position) = 1;
+				}
+				else if (current_level->entrance.side == 3)
+				{
+					*(current_level->map + (current_level->entrance.position * current_level->width)) = 1;
+				}
+
+				// Give current and next levels an exit side, and position
+				// I'm sure this could be more elegant
+				do {
+					current_level->exit.side = (rand() / (RAND_MAX / 4));
+				} while (current_level->exit.side == current_level->entrance.side);
+
+				// North and South are 0 and 2
+				if (current_level->exit.side % 2 == 0)
+				{
+					// Give the door a 1 dimensional position that sits between the _level's width on the N/S wall.
+					current_level->exit.position = (rand() / (RAND_MAX / (current_level->width - 2))) + 1;
+				} 
+				// East and West are 1 and 3
+				else
+				{
+					current_level->exit.position = (rand() / (RAND_MAX / (current_level->height - 2))) + 1;
+				}
+
+				// Assign positions to the current level and prepare the next level with a side 
+				// (will be placed on map as 'current level' next loop)
+				if (current_level->exit.side == 0)
+				{
+					*(current_level->map + current_level->exit.position) = 2;
+
+				}
+				else if (current_level->exit.side == 1)
+				{
+					*(current_level->map + (current_level->width - 1) + (current_level->exit.position * current_level->width)) = 2;
+				}
+				else if (current_level->exit.side == 2)
+				{
+					*(current_level->map + (current_level->width * (current_level->height - 1)) + current_level->exit.position) = 2;
+				}
+				else if (current_level->exit.side == 3)
+				{
+					*(current_level->map + (current_level->exit.position * current_level->width)) = 2;
+				}
+			}
 		}
-		printf("\n");
+
+		for (int y = 0; y < current_level->height; ++y)
+		{
+			for (int x = 0; x < current_level->width; ++x)
+			{
+				printf("%c ", door_types[*(current_level->map + (y * current_level->width) + x)]);
+			}
+			printf("\n");
+		}
+
+		char key[1];
+		scanf("%s", key);
+
+		if (strcasecmp(key, "N") == 0)
+		{
+			if (current_level->next_level == NULL)
+			{
+				struct level *prev_level = current_level;
+				current_level = malloc(sizeof(struct level));
+				current_level->map = NULL;
+				current_level->prev_level = prev_level;
+				current_level->entrance.side = ((prev_level->exit.side + 2) % 4);
+			}
+			else
+			{
+				current_level = current_level->next_level;
+			}
+			advance = 1;
+		}
+		else if (strcasecmp(key, "P") == 0)
+		{
+			if (current_level->prev_level == NULL)
+			{
+				printf("No previous level\n");
+				advance = 0;
+			}
+			else
+			{
+				struct level *next_level = current_level;
+				current_level = current_level->prev_level;
+				current_level->next_level = next_level;
+				advance = -1;
+			}
+		}
+		else if (strcasecmp(key, "Q") == 0)
+		{
+			return 0;
+		}
+		else
+		{
+			advance = 0;
+		}
 	}
 }
