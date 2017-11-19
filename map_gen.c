3d89207ce3f4bc627c22a03f821232a79fc8dcf1
John McCormick <johnmccormick91@gmail.com>
2017-11-19 19:22:48 +0000

    render_game code is much better now. It begins its loop at the correct map position to render a scene, meaning no cycling through each point on the map to see if it needs to appear, and then ends based on similar calculations. Testing with 10,000 tiles took maybe half a second to load, and then ran smoothly, so all is well in terms of it only dealing only with what's on screen (obviously managing all of those tiles will strain the back end, but it's a good sign that the front end manages well).

--- map_gen.c
+++ map_gen.c
@@ -1,97 +1,94 @@
 #include "map_gen.h"
+#include "map_math.c"
 
-void render_game(struct pixel_buffer *write_buffer, struct game_state *main_game_state)
+void render_game(struct pixel_buffer *buffer, struct game_state *game)
 {
-	int total_pixels = write_buffer->client_height * write_buffer->texture_pitch;
+	int total_pixels = buffer->client_height * buffer->texture_pitch;
 
 	// Clear background
-	uint8_t *byte = (uint8_t *)write_buffer->pixels;
+	uint8_t *byte = (uint8_t *)buffer->pixels;
 	for (int i = 0; i < total_pixels; ++i)
 	{
 		*byte++ = 0x00000000;
 	}
 
+	int half_client_width = buffer->client_width / 2;	
+	int half_client_height = buffer->client_height / 2;
 
-	int half_client_width = write_buffer->client_width / 2;	
-	int half_client_height = write_buffer->client_height / 2;
+	int player_tiled_x = (game->player_1.x * tile_size) + tile_size / 2;
+	int player_tiled_y = (game->player_1.y * tile_size) + tile_size / 2;
 
-//	int half_tiled_level_width = tile_size * main_game_state->current_level->width / 2;
-//	int half_tiled_level_height = tile_size * main_game_state->current_level->height / 2;
-
-	int player_tiled_x = (main_game_state->player_1.x * tile_size) + tile_size / 2;
-	int player_tiled_y = (main_game_state->player_1.y * tile_size) + tile_size / 2;
-
-
-	uint8_t *p_location = (uint8_t *)write_buffer->pixels;
-	int px_offset = ((half_client_width - player_tiled_x) * write_buffer->bytes_per_pixel);
-	int py_offset = ((half_client_height - player_tiled_y) * write_buffer->texture_pitch);
+	uint8_t *p_location = (uint8_t *)buffer->pixels;
+	int px_offset = ((half_client_width - player_tiled_x) * buffer->bytes_per_pixel);
+	int py_offset = ((half_client_height - player_tiled_y) * buffer->texture_pitch);
 
 	if (px_offset >= 0)
 		p_location += px_offset;
 	if (py_offset >= 0)
 		p_location += py_offset;
 
+	int x_start;
+	int x_end;
+	
+	x_start = make_neg_zero(player_tiled_x - half_client_width);
+
+	if (x_start + buffer->client_width - make_neg_zero(px_offset / buffer->bytes_per_pixel) > game->current_level->width * tile_size)
+	{
+		x_end = game->current_level->width * tile_size;
+	}
+	else
+	{
+		x_end = x_start + buffer->client_width - make_neg_zero(px_offset / buffer->bytes_per_pixel);
+	}
+
 	int y_start;
 	int y_end;
+	
+	y_start = make_neg_zero(player_tiled_y - half_client_height);
 
-	if (player_tiled_y - half_client_height > 0)
-	{		
-		y_start = (player_tiled_y - half_client_height) / tile_size;
+	if (y_start + buffer->client_height - make_neg_zero(py_offset / buffer->texture_pitch) > game->current_level->height * tile_size)
+	{
+		y_end = game->current_level->height * tile_size;
 	}
 	else
 	{
-		y_start = 0;
+		y_end = y_start + buffer->client_height - make_neg_zero(py_offset / buffer->texture_pitch);
 	}
-	if ((y_start * tile_size) + write_buffer->client_height < main_game_state->current_level->height * tile_size)
-	{
-		y_end = y_start + (write_buffer->client_height / tile_size);
-	}
-	else
-	{
-		y_end = main_game_state->current_level->height;
-	}
+
 	for (int y = y_start; y < y_end; ++y)
 	{
-		for (int height_pixels = 0; height_pixels < tile_size; ++height_pixels)
+		uint32_t *pixel = (uint32_t *) p_location;
+		for (int x = x_start; x < x_end; ++x)
 		{
-			uint32_t *pixel = (uint32_t *) p_location;
-			int x = 0;
-			for (x = 0; x < main_game_state->current_level->width; ++x)
+			if (game->player_1.x == x / tile_size && game->player_1.y == y / tile_size)
+				*pixel++ = 0x000000ff;
+			else
 			{
-				for (int width_pixels = 0; width_pixels < tile_size; ++width_pixels)
-				{
-				
-					if (main_game_state->player_1.x == x && main_game_state->player_1.y == y)
-						*pixel++ = 0x000000ff;
-					else
-					{
-						if (*(main_game_state->current_level->map + (y * main_game_state->current_level->width) + x) == 1)
-							// Floor
-							*pixel++ = 0x00ffffff;
-						else if 
-							(*(main_game_state->current_level->map + (y * main_game_state->current_level->width) + x) == 2)
-							// Entrance
-							*pixel++ = 0x00999999;
-						else if 
-							(*(main_game_state->current_level->map + (y * main_game_state->current_level->width) + x) == 3)
-							// Exit
-							*pixel++ = 0x0000e600;
-						else
-							//Wall
-							*pixel++ = 0x00222222;
-					}
-
-				}
+				if (*(game->current_level->map + ((y / tile_size) * game->current_level->width) + (x / tile_size)) == 1)
+					// Floor
+					*pixel++ = 0x00ffffff;
+				else if 
+					(*(game->current_level->map + ((y / tile_size) * game->current_level->width) + (x / tile_size)) == 2)
+					// Entrance
+					*pixel++ = 0x00999999;
+				else if 
+					(*(game->current_level->map + ((y  / tile_size) * game->current_level->width) + (x / tile_size)) == 3)
+					// Exit
+					*pixel++ = 0x0000e600;
+				else
+					//Wall
+					*pixel++ = 0x00222222;
 			}
-			p_location += write_buffer->texture_pitch;
 		}
+		p_location += buffer->texture_pitch;
 	}
 
 }
 
 struct door new_door_position(struct level **current_level, struct door new_door)
 {
-	// Give the door a 1 dimensional position that sits between the level's width/height on specified wall.
+	// Give the door a 1 dimensional position that sits between 
+	// the level's width and height on specified wall.
 	// North and South are 0 and 2
 	if (new_door.side % 2 == 0)
 	{
@@ -119,10 +116,10 @@ void generate_level (struct level **current_level)
 	// Max w/h should always be >= 3
 	// otherwise there can be no door
 	int MIN_LEVEL_WIDTH = 3; 
-	int MIN_LEVEL_HEIGHT = 15;
+	int MIN_LEVEL_HEIGHT = 3;
 	
-	int MAX_LEVEL_WIDTH = 5;
-	int MAX_LEVEL_HEIGHT = 15;
+	int MAX_LEVEL_WIDTH = 20;
+	int MAX_LEVEL_HEIGHT = 16;
 
 	struct timeval time;
 	gettimeofday(&time, NULL);
@@ -130,8 +127,8 @@ void generate_level (struct level **current_level)
 
 	(*current_level)->next_level = NULL;
 
-	(*current_level)->width = rand() / (RAND_MAX / MAX_LEVEL_WIDTH) + MIN_LEVEL_WIDTH;
-	(*current_level)->height = rand() / (RAND_MAX / MAX_LEVEL_HEIGHT) + MIN_LEVEL_HEIGHT;
+	(*current_level)->width = (rand() % (MAX_LEVEL_WIDTH - MIN_LEVEL_WIDTH + 1)) + MIN_LEVEL_WIDTH;
+	(*current_level)->height = (rand() % (MAX_LEVEL_HEIGHT - MIN_LEVEL_HEIGHT + 1)) + MIN_LEVEL_HEIGHT;
 
 	(*current_level)->map = malloc(sizeof(int) * (*current_level)->width * (*current_level)->height);
 	memset((*current_level)->map, 0, sizeof(int) * (*current_level)->width * (*current_level)->height);
@@ -196,114 +193,114 @@ void prev_level (struct level **current_level)
 	}
 }
 
-void move_player (struct game_state *main_game_state, int x, int y)
+void move_player (struct game_state *game, int x, int y)
 {
 	// If not hitting a wall
-	if (*(main_game_state->current_level->map + ((main_game_state->player_1.y + y) * (main_game_state->current_level->width )+ main_game_state->player_1.x + x)) != 0
+	if (*(game->current_level->map + ((game->player_1.y + y) * (game->current_level->width )+ game->player_1.x + x)) != 0
 		// and not outside game bounds
-		&& main_game_state->player_1.y + y >= 0 && main_game_state->player_1.y + y < main_game_state->current_level->height 
-		&& main_game_state->player_1.x + x >= 0 && main_game_state->player_1.x + x < main_game_state->current_level->width)
+		&& game->player_1.y + y >= 0 && game->player_1.y + y < game->current_level->height 
+		&& game->player_1.x + x >= 0 && game->player_1.x + x < game->current_level->width)
 	{
-		main_game_state->player_1.x += x;
-		main_game_state->player_1.y += y;
+		game->player_1.x += x;
+		game->player_1.y += y;
 
-		main_game_state->player_1.has_moved = true;
+		game->player_1.has_moved = true;
 	}
 	// If going out of bounds
-	else if (main_game_state->player_1.y + y < 0 || main_game_state->player_1.y + y >= main_game_state->current_level->height 
-		|| main_game_state->player_1.x + x < 0 || main_game_state->player_1.x + x >= main_game_state->current_level->width)
+	else if (game->player_1.y + y < 0 || game->player_1.y + y >= game->current_level->height 
+		|| game->player_1.x + x < 0 || game->player_1.x + x >= game->current_level->width)
 	{
 		// If on exit
-		if ((main_game_state->player_1.x == main_game_state->current_level->exit.x) && (main_game_state->player_1.y == main_game_state->current_level->exit.y))
+		if ((game->player_1.x == game->current_level->exit.x) && (game->player_1.y == game->current_level->exit.y))
 		{
-			next_level(&(main_game_state->current_level));
+			next_level(&(game->current_level));
 
-			main_game_state->player_1.x = main_game_state->current_level->entrance.x;
-			main_game_state->player_1.y = main_game_state->current_level->entrance.y;
+			game->player_1.x = game->current_level->entrance.x;
+			game->player_1.y = game->current_level->entrance.y;
 
-			main_game_state->player_1.has_moved = false;
+			game->player_1.has_moved = false;
 		}
 		// If on entrance
-		else if ((main_game_state->player_1.x == main_game_state->current_level->entrance.x) && (main_game_state->player_1.y == main_game_state->current_level->entrance.y))
+		else if ((game->player_1.x == game->current_level->entrance.x) && (game->player_1.y == game->current_level->entrance.y))
 		{
-			prev_level(&(main_game_state->current_level));
+			prev_level(&(game->current_level));
 			
-			main_game_state->player_1.x = main_game_state->current_level->exit.x;
-			main_game_state->player_1.y = main_game_state->current_level->exit.y;
+			game->player_1.x = game->current_level->exit.x;
+			game->player_1.y = game->current_level->exit.y;
 
-			main_game_state->player_1.has_moved = false;
+			game->player_1.has_moved = false;
 		}
 	}
 }
 
-void main_game_loop (struct pixel_buffer *write_buffer, void *game_memory, struct input_events input)
+void main_game_loop (struct pixel_buffer *buffer, void *game_memory, struct input_events input)
 {
 
-	struct game_state *main_game_state = game_memory;
+	struct game_state *game = game_memory;
 
-	if (!main_game_state->initialised)
+	if (!game->initialised)
 	{
-		main_game_state->current_level = malloc(sizeof(struct level));
-		main_game_state->current_level->prev_level = NULL;
-		generate_level(&(main_game_state->current_level));
+		game->current_level = malloc(sizeof(struct level));
+		game->current_level->prev_level = NULL;
+		generate_level(&(game->current_level));
 
-		main_game_state->player_1.x = main_game_state->current_level->width / 2;
-		main_game_state->player_1.y = main_game_state->current_level->height / 2;
+		game->player_1.x = game->current_level->width / 2;
+		game->player_1.y = game->current_level->height / 2;
 
-		main_game_state->initialised = true;
+		game->initialised = true;
 	}
 
 	if (input.keyboard_space)
 	{
-		if (main_game_state->paused == true)
-			main_game_state->paused = false;
+		if (game->paused == true)
+			game->paused = false;
 		else
-			main_game_state->paused = true;
+			game->paused = true;
 	}
 
-	if (!main_game_state->paused)
+	if (!game->paused)
 	{
 
 		if (input.keyboard_up)
 		{
-			move_player(main_game_state, 0, -1);
+			move_player(game, 0, -1);
 		}
 		if (input.keyboard_down)
 		{
-			move_player(main_game_state, 0, 1);
+			move_player(game, 0, 1);
 		}
 		if (input.keyboard_left)
 		{
-			move_player(main_game_state, -1, 0);
+			move_player(game, -1, 0);
 		}
 		if (input.keyboard_right)
 		{
-			move_player(main_game_state, 1, 0);
+			move_player(game, 1, 0);
 		}
 
 		// Move to next/prev level
-		if (main_game_state->player_1.has_moved)
+		if (game->player_1.has_moved)
 		{
-			if ((main_game_state->player_1.x == main_game_state->current_level->exit.x) && (main_game_state->player_1.y == main_game_state->current_level->exit.y))
+			if ((game->player_1.x == game->current_level->exit.x) && (game->player_1.y == game->current_level->exit.y))
 			{
-				next_level(&(main_game_state->current_level));
+				next_level(&(game->current_level));
 				
-				main_game_state->player_1.x = main_game_state->current_level->entrance.x;
-				main_game_state->player_1.y = main_game_state->current_level->entrance.y;
+				game->player_1.x = game->current_level->entrance.x;
+				game->player_1.y = game->current_level->entrance.y;
 
-				main_game_state->player_1.has_moved = false;
+				game->player_1.has_moved = false;
 			} 
-			else if ((main_game_state->player_1.x == main_game_state->current_level->entrance.x) && (main_game_state->player_1.y == main_game_state->current_level->entrance.y))
+			else if ((game->player_1.x == game->current_level->entrance.x) && (game->player_1.y == game->current_level->entrance.y))
 			{
-				prev_level(&(main_game_state->current_level));
+				prev_level(&(game->current_level));
 				
-				main_game_state->player_1.x = main_game_state->current_level->exit.x;
-				main_game_state->player_1.y = main_game_state->current_level->exit.y;
+				game->player_1.x = game->current_level->exit.x;
+				game->player_1.y = game->current_level->exit.y;
 
-				main_game_state->player_1.has_moved = false;
+				game->player_1.has_moved = false;
 			}
 		}
 
-		render_game(write_buffer, main_game_state);
+		render_game(buffer, game);
 	}
 }
