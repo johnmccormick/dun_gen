2051585c4ad503be137c0f4e2719cc762bcc3d32
John McCormick <johnmccormick91@gmail.com>
2017-12-12 04:55:31 +0000

    Fixed bug where collision code was not working correctly for odd player width/height due to integer rounding.
    Also updated render round code, which rounds to the player pixel minus
    t-epsilon*2, to account for the pixel integer rounding up and adding a
    pixel of padding to the left/top of the player when an odd width/height.
    When at a tile boundary, player is .5 distance away, meaning the pixel
    rounds up to 1, adding padding/overflowing with integer rounding on
    the render code.

--- map_gen.c
+++ map_gen.c
@@ -36,11 +36,13 @@ uint32_t get_tile_colour (int tile_value, float level_render_gradient, uint32_t 
 	return colour;
 }
 
+#define pixel_epsilon 0.0001f
+
 void render_game(struct pixel_buffer *buffer, struct game_state *game)
 {
 	// Converts player map coordinate to centered position in terms of pixels
-	int player_1_x = round((game->player_1.position.tile_x * game->tile_size) + game->player_1.position.pixel_x);
-	int player_1_y = round((game->player_1.position.tile_y * game->tile_size) + game->player_1.position.pixel_y);
+	int player_1_x = round((game->player_1.position.tile_x * game->tile_size) + (game->player_1.position.pixel_x) - (pixel_epsilon * 2));
+	int player_1_y = round((game->player_1.position.tile_y * game->tile_size) + (game->player_1.position.pixel_y) - (pixel_epsilon * 2));
 
 	int levels_to_render = 1;
 	int levels_rendered = 0;
@@ -515,7 +517,7 @@ bool test_wall_collision (float x_diff, float delta_x, float top_y, float bottom
 {
 	bool result = false;
 
-	float t_epsilon = 0.00001f;
+	float t_epsilon = pixel_epsilon;
 
 	if (delta_x != 0.0f)
 	{
@@ -684,8 +686,8 @@ void main_game_loop (struct pixel_buffer *buffer, struct memory_block platform_m
 		game->player_1.position.pixel_x = game->tile_size / 2;
 		game->player_1.position.pixel_y = game->tile_size / 2;
 
-		game->player_1.pixel_width = 8;
-		game->player_1.pixel_height = 8;
+		game->player_1.pixel_width = 7;
+		game->player_1.pixel_height = 7;
 
 		// Prep next level
 		game->current_level->next_level = generate_level(&game->world_memory, game->current_level);
@@ -806,8 +808,8 @@ void main_game_loop (struct pixel_buffer *buffer, struct memory_block platform_m
 						int tile_to_player_y = tile_y - game->player_1.position.tile_y;
 
 						// Add half player diameter to wall borders for Minkowski collision
-						float half_player_width = game->player_1.pixel_width / 2;
-						float half_player_height = game->player_1.pixel_height / 2;
+						float half_player_width = (float)game->player_1.pixel_width / 2.0f;
+						float half_player_height = (float)game->player_1.pixel_height / 2.0f;
 
 						float tile_top_left_x = (tile_to_player_x * game->tile_size) - game->player_1.position.pixel_x - half_player_width;
 						float tile_top_left_y = (tile_to_player_y * game->tile_size) - game->player_1.position.pixel_y - half_player_height;
